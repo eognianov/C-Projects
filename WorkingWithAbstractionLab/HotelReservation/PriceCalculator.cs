65bdfd3251f34f027ba8faa590927af9fd4ae3ec
eognianov <emilian_89@abv.bg>
2018-02-16 16:46:17 +0200

    workingWithAbstractionLabFinal

---
+++ WorkingWithAbstractionLab/HotelReservation/PriceCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class PriceCalculator
+{
+    public decimal Price { get; set; }
+
+    public void Calculte(decimal pricePerDay, int numberOfDays, string season, string discountStr)
+    {
+        int addPerSeason = AddPerSeaon(season);
+        int discountPercentege = DiscountPersentage(discountStr);
+        decimal price = pricePerDay * numberOfDays * addPerSeason;
+        decimal discount = price * discountPercentege / 100;
+        this.Price = price - discount;
+    }
+
+    private int DiscountPersentage(string discount)
+    {
+        switch (discount)
+        {
+            case "VIP":
+                return 20;
+            case "SecondVisit":
+                return 10;
+            default:
+                return 0;
+        }
+    }
+
+    private int AddPerSeaon(string season)
+    {
+        switch (season)
+        {
+            case "Autumn":
+                return 1;
+                break;
+            case "Sprint":
+                return 2;
+                break;
+            case "Winter":
+                return 3;
+                break;
+            case "Summer":
+                return 4;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Price:F2}";
+    }
+}
+
--- WorkingWithAbstractionLab/HotelReservation/Program.cs
+++ WorkingWithAbstractionLab/HotelReservation/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace HotelReservation
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string[] inputData = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        decimal pricePerDay = decimal.Parse(inputData[0]);
+        int countOfDays = int.Parse(inputData[1]);
+        string season = inputData[2];
+        string discount = "None";
+        if (inputData.Length==4)
         {
-            Console.WriteLine("Hello World!");
+            discount = inputData[3];
         }
+        PriceCalculator calculator = new PriceCalculator();
+        calculator.Calculte(pricePerDay,countOfDays,season,discount);
+        Console.WriteLine(calculator);
     }
 }
+
