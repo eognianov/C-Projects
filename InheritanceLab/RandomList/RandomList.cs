eb65861a6c4d6b9b234e9d50188c98ce0e803f1c
eognianov <emilian_89@abv.bg>
2018-02-24 03:56:20 +0200

    InheritanceLab all

---
+++ InheritanceLab/RandomList/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class RandomList:List<string>
+{
+    Random random = new Random();
+    public string RandomString()
+    {
+
+        string results = null;
+        if (this.Count>0)
+        {
+            var randomIndex = random.Next(0, this.Count - 1);
+            results = this[randomIndex];
+            this.RemoveAt(randomIndex);
+
+        }
+        return results;
+    }
+}
+
