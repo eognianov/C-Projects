725b4b7be77001b1aec3d1778fddecdf161fc5aa
eognianov <emilian_89@abv.bg>
2018-03-23 17:09:05 +0200

    GenericsLab

---
+++ C# OOP Advanced/GenericsLab/BoxOfT/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box<T>
+{
+    private Stack<T> items;
+
+    public Box()
+    {
+        this.items = new Stack<T>();
+    }
+
+    public int Count => this.items.Count;
+
+    public void Add(T item)
+    {
+        this.items.Push(item);
+    }
+
+    public T Remove()
+    {
+        return this.items.Pop();
+    }
+}
+
