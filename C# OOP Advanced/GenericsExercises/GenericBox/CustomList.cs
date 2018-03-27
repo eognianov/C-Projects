5309160893a1d9eca18954dab858152feaac8e4b
eognianov <emilian_89@abv.bg>
2018-03-27 15:41:16 +0300

    GenericsExercises

---
+++ C# OOP Advanced/GenericsExercises/GenericBox/CustomList.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class CustomList<T>:IEnumerable<T> where T: IComparable<T>
+{
+    private T[] data;
+
+    public CustomList()
+    {
+        this.data  = new T[0];
+    }
+
+    public CustomList(int intilialSize)
+    {
+        this.data = new T[intilialSize];
+    }
+
+    private bool isEmpty => this.Count == 0;
+
+    public int InnerArraySize => this.data.Length;
+
+    public int Count { get; private set; } = 0;
+
+    public T this[int index]
+    {
+        get
+        {
+            return this.data[index];
+
+        }
+        set
+        {
+            this.data[index] = value;
+
+        }
+    }
+
+    public void Add(T element)
+    {
+        int newDataSize = this.isEmpty ? 4 : this.InnerArraySize * 2;
+        this.Count++;
+        if (this.Count>this.InnerArraySize)
+        {
+            T[] newData = new T[newDataSize];
+
+            Array.Copy(this.data, newData, this.InnerArraySize);
+        }
+
+        this.data[this.Count - 1] = element;
+    }
+
+    public T Remove(int index)
+    {
+        T element = this.data[index];
+
+        this.Count--;
+
+        for (int i = index; i < this.Count; i++)
+        {
+            this.data[i] = this.data[i + 1];
+        }
+
+        this.data[this.Count] = default(T);
+
+        if (this.Count<this.InnerArraySize/3)
+        {
+            T[] newData = new T[this.InnerArraySize/2];
+            Array.Copy(this.data, newData, this.Count);
+            this.data = newData;
+        }
+
+        return element;
+    }
+
+    public bool Contains(T element)
+    {
+        for (int i = 0; i < this.Count; i++)
+        {
+            if (this.data[i].Equals(element))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void Swap(int firstIndex, int secondIndex)
+    {
+        T temp = this.data[firstIndex];
+        this.data[firstIndex] = this.data[secondIndex];
+        this.data[secondIndex] = temp;
+    }
+
+    public int CountGreaterThan(T element)
+    {
+        int counter = 0;
+        for (int i = 0; i < this.Count; i++)
+        {
+            T currentEl = this.data[i];
+            if (currentEl.CompareTo(element)>0)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public void Sort(IComparer<T> comparer = null)
+    {
+        Array.Sort(this.data,0,this.Count, comparer);
+    }
+
+    public T Min()
+    {
+        T minElement = this.data[0];
+
+        for (int i = 1; i < this.Count; i++)
+        {
+            T currentEl = this.data[i];
+            if (currentEl.CompareTo(minElement)<0)
+            {
+                minElement = currentEl;
+            }
+        }
+
+        return minElement;
+    }
+
+    public T Max()
+    {
+        T maxElement = this.data[0];
+
+        for (int i = 1; i < this.Count; i++)
+        {
+            T currentEl = this.data[i];
+            if (currentEl.CompareTo(maxElement) > 0)
+            {
+                maxElement = currentEl;
+            }
+        }
+
+        return maxElement;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.Count; i++)
+        {
+            yield return this.data[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
+
---
+++ C# OOP Advanced/GenericsExercises/GenericBox/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //TaskCustomList();
+            //TaskTuple();
+            TaskThreeuple();
+        }
+
+        private static void TaskCustomList()
+        {
+            CustomList<string> list = new CustomList<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = input.Split();
+                string command = commandArgs[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        list.Add(commandArgs[1]);
+                        break;
+                    case "Remove":
+                        int index = int.Parse(commandArgs[1]);
+                        list.Remove(index);
+                        break;
+                    case "Contains":
+                        bool result = list.Contains(commandArgs[1]);
+                        Console.WriteLine(result);
+                        break;
+                    case "Swap":
+                        int firtIndex = int.Parse(commandArgs[1]);
+                        int secondIndex = int.Parse(commandArgs[2]);
+                        list.Swap(firtIndex, secondIndex);
+                        break;
+                    case "Greater":
+                        int count = list.CountGreaterThan(commandArgs[1]);
+                        Console.WriteLine(count);
+                        break;
+                    case "Min":
+                        string minElement = list.Min();
+                        Console.WriteLine(minElement);
+                        break;
+                    case "Max":
+                        string maxElement = list.Max();
+                        Console.WriteLine(maxElement);
+                        break;
+                    case "Print":
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            Console.WriteLine(list[i]);
+                        }
+
+                        break;
+                    case "Sort":
+                        list.Sort();
+                        break;
+                }
+            }
+        }
+
+        public static void TaskTuple()
+        {
+            var input = Console.ReadLine().Split();
+            var name = $"{input[0]} {input[1]}";
+            var address = input[2];
+
+            Console.WriteLine(new Tuple<string, string>(name, address));
+
+            input = Console.ReadLine().Split();
+            name = input[0];
+            var litersOfBeer = int.Parse(input[1]);
+
+            Console.WriteLine(new Tuple<string, int>(name, litersOfBeer));
+
+            input = Console.ReadLine().Split();
+            var integer = int.Parse(input[0]);
+            var doubleValue = double.Parse(input[1]);
+
+            Console.WriteLine(new Tuple<int, double>(integer, doubleValue));
+        }
+
+        public static void TaskThreeuple()
+        {
+            var input = Console.ReadLine().Split();
+            Console.WriteLine(new Threeuple<string, string, string>($"{input[0]} {input[1]}", input[2], input[3]));
+
+            input = Console.ReadLine().Split();
+            var drunkResult = !input[2].Equals("drunk") ? "False" : "True";
+            Console.WriteLine(new Threeuple<string, int, bool>(input[0], int.Parse(input[1]), bool.Parse(drunkResult)));
+
+            input = Console.ReadLine().Split();
+
+            Console.WriteLine(new Threeuple<string, double, string>(input[0], double.Parse(input[1]), input[2]));
+        }
+    }
+}
