75fc5358b4c036a82c0a4a703504ff0176efc16c
eognianov <emilian_89@abv.bg>
2018-04-05 20:51:07 +0300

    UnitTestLab

--- C# OOP Advanced/UnitTestingLab/Tests/AxeTests.cs
+++ C# OOP Advanced/UnitTestingLab/Tests/AxeTests.cs
@@ -15,5 +15,13 @@ namespace Tests
 
             Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
         }
+
+        [Test]
+        public void BrokenAxeCannotAttack()
+        {
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 10);
+            Assert.That(()=> axe.Attack(dummy), Throws.InvalidOperationException.With.Message.EqualTo("Axe is broken."));
+        }
     }
 }
---
+++ C# OOP Advanced/UnitTestingLab/Tests/DummyTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            dummy.TakeAttack(1);
+            Assert.That(dummy.Health, Is.EqualTo(9), "Dummy dosn't take demage.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExecptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+            Assert.That(() => dummy.TakeAttack(5), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveExpirience()
+        {
+            Dummy dummy = new Dummy(0, 10);
+            Assert.That(() => dummy.GiveExperience(), Is.EqualTo(10), "Target is not dead.");
+        }
+
+        [Test]
+        public void AliveDummyCannotGiveExpirience()
+        {
+            Dummy dummy = new Dummy(5, 10);
+            Assert.That(() => dummy.GiveExperience(), Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+        }
+    }
+}
