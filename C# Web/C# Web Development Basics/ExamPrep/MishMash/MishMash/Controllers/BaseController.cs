3131cb03976bf741ee20cd7f99893a0897e4112e
eognianov <emilian_89@abv.bg>
2018-10-30 13:20:26 +0200

    Implement Login Register And Logout

---
+++ C# Web/C# Web Development Basics/ExamPrep/MishMash/MishMash/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MishMash.Data;
+using SIS.MvcFramework;
+
+namespace MishMash.Controllers
+{
+    public class BaseController:Controller
+    {
+        public BaseController()
+        {
+            this.Db = new ApplicationDbContext();
+        }
+
+        protected ApplicationDbContext Db { get; }
+    }
+
+    
+}
---
+++ C# Web/C# Web Development Basics/ExamPrep/MishMash/MishMash/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+
+namespace MishMash.Controllers
+{
+    public class HomeController:BaseController
+    {
+        [HttpGet("/Home/Index")]
+        public IHttpResponse Index()
+        {
+            return this.View("Home/Index");
+        }
+
+        [HttpGet("/")]
+        public IHttpResponse RootIndex()
+        {
+            return this.View("Home/Index");
+        }
+    }
+}
