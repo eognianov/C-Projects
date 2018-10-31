377643bfb099db29961952db62ac9337b6ddc576
eognianov <emilian_89@abv.bg>
2018-10-31 18:09:54 +0200

    small changes

---
+++ C# Web/C# Web Development Basics/ExamPrep/MishMash/MishMash/ViewModels/Home/LoggedInIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MishMash.ViewModels.Channels;
+
+namespace MishMash.ViewModels.Home
+{
+    public class LoggedInIndexViewModel
+    {
+        public string UserRole { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> YourChannels { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> SuggestedChannels { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> SeeOtherChannels { get; set; }
+    }
+}
